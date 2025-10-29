1e0ff3ae67eb1ce3dfc7050f31a81796366b63b1
Daniel Andrade <dan.andrade313@gmail.com>
2025-10-29 14:17:20 -0300

    resolvido exercicio01

---
+++ aula01/Exercicio01.js
@@ -0,0 +1,10 @@
+const leia = require("readline-sync")
+
+let salario, abono, novoSalario
+
+salario = leia.questionFloat("Digite o salário: ")
+abono = leia.questionFloat("Digite o abono: ")
+
+novoSalario = salario + abono
+
+console.log("O novo salário é: " + novoSalario)
