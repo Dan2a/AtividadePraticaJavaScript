3bf6106321016663bd64b8c6b6e387cdcf210c2d
Daniel Andrade <dan.andrade313@gmail.com>
2025-10-31 14:43:35 -0300

    resolvido exercio01-aula03

---
+++ aula03_lacosRepeticao_vetores/Exerciocio01_For.js
@@ -0,0 +1,15 @@
+const leia = require("readline-sync");
+
+let n1, n2
+n1 = leia.questionInt("Digite o primeiro numero do intervalo: ");
+n2 = leia.questionInt("Digite o segundo numero do intervalo: ");
+
+if (n1 > n2) {
+  console.log("Intervalo inválido! O primeiro número deve ser menor que o segundo.");
+} else {
+    for(let i = n1; i <= n2; i++) {
+        if(i%3 == 0 && i%5 == 0) {
+            console.log(`${i} é múltiplo de 3 ou 5`);
+        }
+    }
+}
