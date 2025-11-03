74786c32cc5c72437e19c6ab40e27d34f8519571
Daniel Andrade <dan.andrade313@gmail.com>
2025-11-03 13:57:15 -0300

    inicializado typeScript e resolvido exercicio01_aula04

---
+++ aula04_ArraySet/Exercicio01_Array.ts
@@ -0,0 +1,21 @@
+import leia = require("readline-sync");
+
+let cores: Array<string> = new Array<string>();
+
+for(let i = 0; i < 5; i++){
+    let cor = leia.question("Digite uma cor: ");
+    cores.push(cor);
+}
+
+console.log("\nLista todas as cores:\n");
+
+for(let cor of cores){
+    console.log(cor);
+}
+
+cores.sort();
+
+console.log("\nOrdenar as cores:\n");
+for(let cor of cores){
+    console.log(cor);
+}
